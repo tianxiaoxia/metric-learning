d03276eba2e7b16be9aaf5109d6046ca273b3295
tianxiaoxia <13991234763@163.com>
2018-11-29 15:28:50 +0800

    Update task_generator.py

--- task_generator.py
+++ task_generator.py
@@ -23,3 +23,29 @@ def omniglot_character_folders():
     metaval_character_folders = character_folders[num_train:]
 
     return metatrain_character_folders,metaval_character_folders
+
+def get_class(sample):
+    return os.path.join(*sample.split('\\')[:-1])
+
+def omniglotTask(character_folders, num_classes, support_set_num, target_set_num): 
+    # character_folders denotes metatrain_character_folders or metaval_character_folders
+    class_folders = random.sample(character_folders, num_classes)  
+    # print(class_folders)
+    labels = np.array(range(len(class_folders)))
+    labels = dict(zip(class_folders, labels))
+    
+    samples = {}
+    support_set = []
+    target_set = []
+    
+    for c in class_folders:
+        temp = [os.path.join(c, x) for x in os.listdir(c)]  
+        samples[c] = random.sample(temp, len(temp))   
+
+        support_set += samples[c][:support_set_num]
+        target_set += samples[c][support_set_num:support_set_num + target_set_num]
+    
+    support_set_labels = [labels[get_class(x)] for x in support_set]
+    target_set_labels = [labels[get_class(x)] for x in target_set]
+    
+    
